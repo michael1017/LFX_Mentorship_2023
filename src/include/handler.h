324303c3b9edfa14ff61553298a65fa566974fda
michael1017 <michael881017@gmail.com>
2023-02-03 20:25:33 +0800

    split option handler

---
+++ src/include/handler.h
@@ -0,0 +1,11 @@
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include "argparse.h"
+#include "option.h"
+
+bool handle_option_version(void);
+bool handle_option_wasm_arg(const Option* );
+bool handle_option(const ParseData* );
+
+#endif /* HANDLER_H */
