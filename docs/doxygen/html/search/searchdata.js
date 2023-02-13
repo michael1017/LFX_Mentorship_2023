999cfb35c2402ba3d5733a1254eda10ad7a14bbc
michael1017 <michael881017@gmail.com>
2023-02-13 16:44:01 +0000

    add html support

---
+++ docs/doxygen/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "op",
+  1: "op"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
